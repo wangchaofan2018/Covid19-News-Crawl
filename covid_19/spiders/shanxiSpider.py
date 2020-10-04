78c723ac4490c65e099f72e0687447eacea113c1
wangchaofan <447192078@qq.com>
2020-10-04 16:28:33 +0800

    shanxiSpider 开发完成

--- covid_19/spiders/shanxiSpider.py
+++ covid_19/spiders/shanxiSpider.py
@@ -14,50 +14,40 @@ class ShanxiSpider(Spider):
         chrome_options.add_argument("start-maximized")
         chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
         chrome_options.add_experimental_option('useAutomationExtension', False)
-        # chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--headless')
         self.browser = webdriver.Chrome('chromedriver', chrome_options=chrome_options)
         super(ShanxiSpider, self).__init__()
+        self.since = 1
 
     name="shanxi"
 
-    since = 1
     # start_urls = ["http://www.shanxi.gov.cn/yw/xwfbh/szfxwfbh/index.shtml"]
     base_url = "http://www.shanxi.gov.cn/yw/xwfbh/szfxwfbh"
     def start_requests(self):
         start_url = "http://www.shanxi.gov.cn/yw/xwfbh/szfxwfbh/index.shtml"
         yield scrapy.Request(url=start_url,meta={'use_browser':True},callback=self.list_parse,dont_filter=True)
 
-    # def parse(self,response):
-    #     sel = Selector(response)
-    #     total_page_str = sel.xpath('//span[@class="shanxi-gov-page-form"]').extract_first()
-    #     total_page_obj = re.match(".*?[\u4e00-\u9fa5]+(\d+)[\u4e00-\u9fa5]+",total_page_str)
-    #     if total_page_obj:
-    #         total_page_str = total_page_obj.group(1)
-    #         total_page = int(total_page_str)
-    #         for i in range(total_page):
-    #             if i > 0:
-    #                 next_url = self.base_url + "/index_"+i+".shtml"
-    #                 yield scrapy.Request(url=next_url,meta={'use_browser':True},callback=self.list_parse,dont_filter=True)
-    #             else:
-    #                 yield scrapy.Request(url="http://www.shanxi.gov.cn/yw/xwfbh/szfxwfbh/index.shtml",meta={'use_browser':True},callback=self.list_parse,dont_filter=True)
-
     def list_parse(self,response):
         sel = Selector(response)
         hrefs = sel.xpath("//ul[@class='common-tab-content-box ftsz-16 mgtp-0 common-list-box']/li/a/@href").extract()
         total_page_str = sel.xpath('//span[@class="shanxi-gov-page-form"]').extract_first()
         total_page_obj = re.match(".*?[\u4e00-\u9fa5]+(\d+)[\u4e00-\u9fa5]+",total_page_str)
+        print("first1")
+        if total_page_obj:
+            print("first2")
+            total_page_str = total_page_obj.group(1)
+            total_page = int(total_page_str)
+            if self.since<total_page:
+                print("first3")
+                next_url = self.base_url + "/index_"+str(self.since)+".shtml"
+                self.since+=1
+                yield scrapy.Request(url=next_url,meta={'use_browser':True},callback=self.list_parse,dont_filter=True)
+
         for href_str in hrefs:
             href = href_str[1:]
             next_url = self.base_url+href
             yield scrapy.Request(url=next_url,meta={"detail_url":next_url,'use_browser':True},callback=self.detail_parse,dont_filter=True)
-        if total_page_obj:
-            total_page_str = total_page_obj.group(1)
-            total_page = int(total_page_str)
-            if self.since<total_page:
-                next_url = self.base_url + "/index_"+self.since+".shtml"
-                self.since= self.since+1
-                yield scrapy.Request(url=next_url,meta={'use_browser':True},callback=self.list_parse,dont_filter=True)
-           
+        
 
 
     def detail_parse(self,response):
@@ -72,7 +62,7 @@ class ShanxiSpider(Spider):
         for person in person_arr:
             if person.find("记者")==-1:
                 attend_persons = attend_persons+person
-        summary = sel.xpath('//p[@align="center"]/following-sibling::p[1]/text()').extract_first().strip()
+        summary = sel.xpath('//p[@align="center"]/following-sibling::p[1]/text()').extract_first()
         content = ""
         content_strs = sel.xpath('//div[@class="TRS_Editor"]/p/text()').extract()
         for content_row in content_strs:
@@ -85,7 +75,7 @@ class ShanxiSpider(Spider):
         item['summary'] = summary
         item['content'] = content
         item['detail_url'] = detail_url
-        # yield item
+        yield item
         
 
         
