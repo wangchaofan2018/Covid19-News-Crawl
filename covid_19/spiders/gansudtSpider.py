6791841d64e57cff86507634d8e99f3324c5861c
wangchaofan <447192078@qq.com>
2020-10-13 23:55:38 +0800

    修改gansunewsSpider==>gansuSpider&修复gansudt问题“

--- covid_19/spiders/gansudtSpider.py
+++ covid_19/spiders/gansudtSpider.py
@@ -7,25 +7,39 @@ from covid_19.items import BaseDataItem
 
 class GansuDtSpider(Spider):
     def __init__(self):
-        
+        self.data_param = {
+            'webid':"1",
+            'columnid':"10497",
+            'unitid':"1342"
+        }
+        self.since = 0
         super(GansuDtSpider, self).__init__()
 
     name = "gansudt"
     original_url = "http://www.gansu.gov.cn"
 
     def start_requests(self):
-        url = "http://www.gansu.gov.cn/col/col10497/index.html"
-        yield scrapy.Request(url=url,callback=self.parse,dont_filter=True)
+        url = "http://www.gansu.gov.cn/module/jslib/jquery/jpage/dataproxy.jsp?startrecord=1&endrecord=120&perpage=40"
+        yield scrapy.FormRequest(url=url,method="POST",formdata=self.data_param ,headers={"Content_type":"application/x-www-form-urlencoded"},callback=self.parse,dont_filter=True)
 
     def parse(self,response):
+        total = int(re.findall(r"totalRecord=(\d+)",response.text,re.M)[0])
         sel = Selector(response)
         deatil_page_info = sel.xpath('//table[@width="96%"]//tr')
         for info in deatil_page_info:
             detail_page_url = info.xpath('.//td[2]/a/@href').extract_first()
-            detail_url = self.original_url + detail_page_url
+            print(detail_page_url)
+            print("***")
+            detail_url = self.original_url + "/" + detail_page_url[2:-2]
             publish_time_all = info.xpath('//td[3]/span/text()').extract_first()  #但时间形式为：[2020-09-09]，需要处理
             publish_time = publish_time_all[1:-1]
             yield scrapy.Request(url=detail_url,meta={"detail_url":detail_url,"publish_time":publish_time},callback=self.detail_parse,dont_filter=True)
+
+        self.since +=120
+        if self.since<total:
+            url = "http://www.gansu.gov.cn/module/jslib/jquery/jpage/dataproxy.jsp?startrecord=%s&endrecord=%s&perpage=40"%(str(self.since+1),str(self.since+120))
+            yield scrapy.FormRequest(url=url,method="POST",formdata=self.data_param ,headers={"Content_type":"application/x-www-form-urlencoded"},callback=self.parse,dont_filter=True)
+        
 
     def detail_parse(self,response):
         item = BaseDataItem()
@@ -33,6 +47,7 @@ class GansuDtSpider(Spider):
         item["detail_url"] = response.meta["detail_url"]
         item["publish_time"] = response.meta["publish_time"]
         item["location"] = "甘肃"
+        item["attend_persons"]=[]
         title = ""
         title_text = sel.xpath('//table[@width="95%"]//tr[1]/td/text()').extract()
         for col in title_text:
@@ -45,8 +60,7 @@ class GansuDtSpider(Spider):
 
         item["content"] = content
         item["summary"] = ""
-        print(item)
-        # yield item
+        yield item
 
 
 
